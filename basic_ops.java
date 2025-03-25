5a3ec662a62903fbd5cb281d155a25381079c7b1
Naga phaneendra puranam <134182485+Phaneendraaa@users.noreply.github.com>
2025-03-25 15:39:52 +0530

    Update basic_ops.java

--- basic_ops.java
+++ basic_ops.java
@@ -2,18 +2,18 @@ import java.util.*;
 public class basic_ops{
        public static void main(String[] args){
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("ENter two nums");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println("Addition of two nums is");
-		System.out.println(a+b);
-		System.out.println("SUbtraction of two nums is");
-		 System.out.println(a-b);
-		System.out.println("Multiplication of two nums is");
-		 System.out.println(a*b);
-		System.out.println("Division of two nums is");
-		 System.out.println(a/b);
+		if (args.length < 2) {
+            System.out.println("Please provide two numbers as command-line arguments.");
+            return;
+        }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("Addition of two nums is: " + (a + b));
+        System.out.println("Subtraction of two nums is: " + (a - b));
+        System.out.println("Multiplication of two nums is: " + (a * b));
+        System.out.println("Division of two nums is: " + (a / b));
 
 
        	}
