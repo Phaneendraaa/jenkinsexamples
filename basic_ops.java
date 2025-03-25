6420b6465219ccee4e0efcb6a0b3f7f3a729cd16
Naga phaneendra puranam <134182485+Phaneendraaa@users.noreply.github.com>
2025-03-25 14:56:33 +0530

    Create basic_ops.java

---
+++ basic_ops.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class basic_ops{
+       public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ENter two nums");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Addition of two nums is");
+		System.out.println(a+b);
+		System.out.println("SUbtraction of two nums is");
+		 System.out.println(a-b);
+		System.out.println("Multiplication of two nums is");
+		 System.out.println(a*b);
+		System.out.println("Division of two nums is");
+		 System.out.println(a/b);
+
+
+       	}
+}
